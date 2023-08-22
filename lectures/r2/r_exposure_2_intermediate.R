542f37ba2ceaec275415a6d079893d3efb7b4241
Charles Lanfear <cclanfear@gmail.com>
2023-08-21 21:05:17 -0700

    stuff

--- lectures/r2/r_exposure_2_intermediate.R
+++ lectures/r2/r_exposure_2_intermediate.R
@@ -1,6 +1,6 @@
 library(gapminder)
 library(dplyr)
-China <- gapminder %>%
+China <- gapminder |>
   filter(country == "China")
 head(China, 4)
 
@@ -141,7 +141,8 @@ ggplot(data = gapminder, #<<
 
 ## ggplot(data = gapminder,
 ##        aes(x = year, y = lifeExp)) +
-##   geom_line(color = "red", size = 3) + #<<
+##   geom_line(color = "red", #<<
+##             linewidth = 3) + #<<
 ##   xlab("Year") +
 ##   ylab("Life expectancy") +
 ##   ggtitle("Life expectancy over time") +
@@ -149,7 +150,8 @@ ggplot(data = gapminder, #<<
 
 ggplot(data = gapminder, 
        aes(x = year, y = lifeExp)) +
-  geom_line(color = "red", size = 3) + #<<
+  geom_line(color = "red", #<<
+            linewidth = 3) + #<<
   xlab("Year") + 
   ylab("Life expectancy") +
   ggtitle("Life expectancy over time") +
@@ -158,7 +160,8 @@ ggplot(data = gapminder,
 ## ggplot(data = gapminder,
 ##        aes(x = year, y = lifeExp,
 ##            group = country)) + #<<
-##   geom_line(color = "red", size = 3) +
+##   geom_line(color = "red", #<<
+##             linewidth = 3) + #<<
 ##   xlab("Year") +
 ##   ylab("Life expectancy") +
 ##   ggtitle("Life expectancy over time") +
@@ -167,7 +170,8 @@ ggplot(data = gapminder,
 ggplot(data = gapminder, 
        aes(x = year, y = lifeExp, 
            group = country)) + #<<
-  geom_line(color = "red", size = 3) +
+  geom_line(color = "red", #<<
+            linewidth = 3) + #<<
   xlab("Year") + 
   ylab("Life expectancy") +
   ggtitle("Life expectancy over time") +
@@ -176,7 +180,8 @@ ggplot(data = gapminder,
 ## ggplot(data = gapminder,
 ##        aes(x = year, y = lifeExp,
 ##            group = country)) +
-##   geom_line(color = "red") + #<<
+##   geom_line(color = "red", #<<
+##             linewidth = 3) + #<<
 ##   xlab("Year") +
 ##   ylab("Life expectancy") +
 ##   ggtitle("Life expectancy over time") +
@@ -185,7 +190,8 @@ ggplot(data = gapminder,
 ggplot(data = gapminder, 
        aes(x = year, y = lifeExp, 
            group = country)) +
-  geom_line(color = "red") + #<<
+  geom_line(color = "red", #<<
+            linewidth = 3) + #<<
   xlab("Year") + 
   ylab("Life expectancy") +
   ggtitle("Life expectancy over time") +
@@ -324,7 +330,7 @@ lifeExp_by_year +
 ggplot(data = China, aes(x = year, y = gdpPercap)) +
     geom_line() +
     scale_y_log10(breaks = c(1000, 2000, 3000, 4000, 5000), #<<
-                  labels = scales::dollar) + #<<
+                  labels = scales::label_dollar()) + #<<
     xlim(1940, 2010) + ggtitle("Chinese GDP per capita")
 
 ## ggsave("I_saved_a_file.pdf", plot = lifeExp_by_year,
@@ -344,49 +350,49 @@ ggplot(data = China, aes(x = year, y = gdpPercap)) +
 ##   theme_bw() + theme(legend.position = c(0.86, 0.15),
 ##                      legend.background = element_rect(color = 1))
 
-yugoslavia <- gapminder %>% filter(country %in% c("Bosnia and Herzegovina",
+yugoslavia <- gapminder |> filter(country %in% c("Bosnia and Herzegovina",
                "Croatia", "Macedonia", "Montenegro", "Serbia", "Slovenia"))
 
-yugoslavia %>% filter(year == 1982) %>%
+yugoslavia |> filter(year == 1982) |>
     summarize(n_obs = n(),
               total_pop = sum(pop),
               mean_life_exp = mean(lifeExp),
               range_life_exp = max(lifeExp) - min(lifeExp))
 
-yugoslavia %>%
-  filter(year == 1982) %>%
+yugoslavia |>
+  filter(year == 1982) |>
   summarize(across(c(lifeExp, pop), list(avg = ~mean(.), sd = ~sd(.))))
 
-## yugoslavia %>%
-##   filter(year == 1982) %>%
+## yugoslavia |>
+##   filter(year == 1982) |>
 ##   summarize(
 ##     across(
 ##       c(lifeExp, pop),
 ##       list(
 ##         avg = ~mean(.),
-##         sd = ~sd(.)
+##         sd  = ~sd(.)
 ##       )
 ##     )
 ##   )
 
-## dataframe %>%
+## dataframe |>
 ##   summarize(across(everything(), list(mean = ~mean(.), sd = ~sd(.))))
 
-## dataframe %>%
+## dataframe |>
 ##   summarize(across(where(is.numeric), list(mean = ~mean(.), sd = ~sd(.))))
 
-yugoslavia %>%
-  group_by(year) %>% #<<
+yugoslavia |>
+  group_by(year) |> #<<
     summarize(total_pop = sum(pop),
-              total_gdp_per_cap = sum(pop*gdpPercap)/total_pop) %>%
+              total_gdp_per_cap = sum(pop*gdpPercap)/total_pop) |>
     head(5)
 
-yugoslavia %>% 
-  select(country, year, pop) %>%
-  filter(year >= 2002) %>% 
-  group_by(country) %>%
+yugoslavia |> 
+  select(country, year, pop) |>
+  filter(year >= 2002) |> 
+  group_by(country) |>
   mutate(lag_pop = lag(pop, order_by = year),
-         pop_chg = pop - lag_pop) %>%
+         pop_chg = pop - lag_pop) |>
   head(4)
 
 ## library(readr) # Contains read_csv()
@@ -409,9 +415,9 @@ library(pander)
 pander(head(billboard_2000_raw[,1:10], 12), split.tables=120, style="rmarkdown")
 
 library(tidyr)
-billboard_2000 <- billboard_2000_raw %>%
+billboard_2000 <- billboard_2000_raw |>
   pivot_longer(starts_with("wk"), 
-               names_to ="week", #<<
+               names_to  = "week", #<<
                values_to = "rank") #<<
 dim(billboard_2000)
 
@@ -419,9 +425,9 @@ head(billboard_2000)
 
 summary(billboard_2000$rank)
 
-billboard_2000 <- billboard_2000_raw %>%
+billboard_2000 <- billboard_2000_raw |>
   pivot_longer(starts_with("wk"), 
-               names_to ="week", 
+               names_to  = "week", 
                values_to = "rank", 
                values_drop_na = TRUE) #<<
 summary(billboard_2000$rank)
@@ -430,23 +436,23 @@ dim(billboard_2000)
 
 summary(billboard_2000$week)
 
-billboard_2000 <- billboard_2000 %>%
+billboard_2000 <- billboard_2000 |>
     mutate(week = parse_number(week)) #<<
 summary(billboard_2000$week)
 
-billboard_2000 <- billboard_2000_raw %>%
+billboard_2000 <- billboard_2000_raw |>
   pivot_longer(starts_with("wk"), 
-               names_to ="week", 
+               names_to  = "week", 
                values_to = "rank",
                values_drop_na = TRUE,
                names_prefix = "wk", #<<
                names_transform = list(week = as.integer))  #<<
 head(billboard_2000, 3)
 
-billboard_2000 <- billboard_2000 %>%
+billboard_2000 <- billboard_2000 |>
     separate(time, into = c("minutes", "seconds"),
-             sep = ":", convert = TRUE) %>% #<<
-    mutate(length = minutes + seconds / 60) %>%
+             sep = ":", convert = TRUE) |> #<<
+    mutate(length = minutes + seconds / 60) |>
     select(-minutes, -seconds)
 summary(billboard_2000$length)
 
@@ -455,15 +461,15 @@ summary(billboard_2000$length)
               Statistic = rep(c("Mean", "Median", "SD"), 2),
               Value     = c(1.28, 1.0, 0.72, 2.81, 2, 1.33)))
 
-(just_right_data <- too_long_data %>%
+(just_right_data <- too_long_data |>
     pivot_wider(names_from = Statistic, values_from = Value))
 
-billboard_2000 <- billboard_2000 %>%
-    group_by(artist, track) %>%
+billboard_2000 <- billboard_2000 |>
+    group_by(artist, track) |>
     mutate(`Weeks at #1` = sum(rank == 1),
            `Peak Rank`   = ifelse(any(rank == 1), #<<
                                   "Hit #1",
-                                  "Didn't #1")) %>%
+                                  "Didn't #1")) |>
     ungroup() #<<
 
 library(ggplot2)
@@ -485,16 +491,16 @@ billboard_trajectories <-
 
 billboard_trajectories
 
-billboard_2000 %>%
-    select(artist, track, `Weeks at #1`) %>%
-    distinct(artist, track, `Weeks at #1`) %>%
-    arrange(desc(`Weeks at #1`)) %>%
+billboard_2000 |>
+    select(artist, track, `Weeks at #1`) |>
+    distinct(artist, track, `Weeks at #1`) |>
+    arrange(desc(`Weeks at #1`)) |>
     head(7)
 
-billboard_2000 <- billboard_2000 %>%
+billboard_2000 <- billboard_2000 |>
     mutate(date = date.entered + (week - 1) * 7) #<<
-billboard_2000 %>% arrange(artist, track, week) %>%
-    select(artist, date.entered, week, date, rank) %>% head(4)
+billboard_2000 |> arrange(artist, track, week) |>
+    select(artist, date.entered, week, date, rank) |> head(4)
 
 plot_by_day <- 
   ggplot(billboard_2000, aes(x = date, y = rank, group = track)) +
@@ -519,33 +525,33 @@ library(nycflights13)
 ## data(airports)
 ## # and so on...
 
-flights %>% filter(dest == "SEA") %>% 
-    select(carrier) %>%
-    left_join(airlines, by = "carrier") %>%
-    count(name) %>% #<<
+flights |> filter(dest == "SEA") |> 
+    select(carrier) |>
+    left_join(airlines, by = "carrier") |>
+    count(name) |> #<<
     arrange(desc(n))
 
-flights %>% filter(dest == "SEA") %>% select(tailnum) %>%
-    left_join(planes %>% select(tailnum, manufacturer), #<<
-              by = "tailnum") %>%
-    count(manufacturer) %>% # Count observations by manufacturer
+flights |> filter(dest == "SEA") |> select(tailnum) |>
+    left_join(planes |> select(tailnum, manufacturer), #<<
+              by = "tailnum") |>
+    count(manufacturer) |> # Count observations by manufacturer
     arrange(desc(n)) # Arrange data descending by count
 
-## flights %>%
-##     select(origin, year, month, day, hour, dep_delay) %>%
+## flights |>
+##     select(origin, year, month, day, hour, dep_delay) |>
 ##     inner_join(weather,
-##            by = c("origin", "year", "month", "day", "hour")) %>%
-##     select(dep_delay, wind_gust) %>%
+##            by = c("origin", "year", "month", "day", "hour")) |>
+##     select(dep_delay, wind_gust) |>
 ##     # removing rows with missing values
-##     filter(!is.na(dep_delay) & !is.na(wind_gust)) %>%
+##     filter(!is.na(dep_delay) & !is.na(wind_gust)) |>
 ##     ggplot(aes(x = wind_gust, y = dep_delay)) +
 ##       geom_point() +
 ##       geom_smooth()
 
-flights %>% select(origin, year, month, day, hour, dep_delay) %>%
-    inner_join(weather, by = c("origin", "year", "month", "day", "hour")) %>%
-    select(dep_delay, wind_gust) %>%
+flights |> select(origin, year, month, day, hour, dep_delay) |>
+    inner_join(weather, by = c("origin", "year", "month", "day", "hour")) |>
+    select(dep_delay, wind_gust) |>
     # removing rows with missing values
-    filter(!is.na(dep_delay) & !is.na(wind_gust)) %>%
+    filter(!is.na(dep_delay) & !is.na(wind_gust)) |>
     ggplot(aes(x = wind_gust, y = dep_delay)) +
       geom_point() + geom_smooth()
